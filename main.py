5f2a0131e9e4ddc5a7905816b8c428e915944261
axolotl-being <nora.and.the.moon@gmail.com>
2023-10-21 19:38:15 -0500

    starting on finding current sales

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ from steam.webapi import WebAPI
 api = WebAPI(key="45D432B97842C610AD4E38ABC4832B3B")
 
 import requests as req
-#from bs4 import BeautifulSoup
 
 wishlist_url = "https://store.steampowered.com/wishlist/profiles/" + str(user) + "/wishlistdata/?p=0"
 
 wishlist_page = req.get(wishlist_url)
 wishlist_data = json.loads(wishlist_page.text)
 with open("wishlist_file.json", "w") as write_file:
-    json.dump(wishlist_data, write_file, separators=(",", ":"))
+    json.dump(wishlist_data, write_file, separators=(",", ":"), indent=10)
 
-#print(wishlist_data)
+current_sales = {}
+for titleid,attributes in wishlist_data.items(): #create nested loop to interact with further dict levels
+    if str(attributes).find('"discount_pct":null') == False:
+        current_sales[titleid]=attributes
+
+print(current_sales.keys())
